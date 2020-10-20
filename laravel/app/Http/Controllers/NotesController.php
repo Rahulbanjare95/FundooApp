22ebb6654cab7010de8f416c4448031f82340d93
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-10-20 09:46:09 +0530

    feat(CreateNote): Creates notes inside database

--- laravel/app/Http/Controllers/NotesController.php
+++ laravel/app/Http/Controllers/NotesController.php
@@ -1,10 +1,63 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
+use App\Models\Notes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
 
 class NotesController extends Controller
 {
-    
+    protected $user;
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+        $this->user = auth()->user();   
+    }
+
+    public function createNote(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'title'     => 'string',
+                'description'=> 'string',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'status' => false,
+                    'errors' => $validator->errors(),
+                ],
+                400
+            );
+        }
+
+        $note            = new Notes();
+        $note->title     = $request->title;
+        $note->description      = $request->description;
+        
+
+        if ($this->user->notes()->save($note)) {
+            return response()->json(
+                [
+                    'status' => true,
+                    'data'   => $note,
+                ]
+            );
+        } else {
+            return response()->json(
+                [
+                    'status'  => false,
+                    'message' => 'Oops, the todo could not be saved.',
+                ]
+            );
+        }
+
+    }
+
 }
